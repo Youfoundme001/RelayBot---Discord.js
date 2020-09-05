3fcc9a381ab340ff0c1a22b9c879dd753a885e7e
YouFoundMe <Youfoundme001@users.noreply.github.com>
2020-09-05 19:58:12 -0400

    Initial Upload
    Please see notes in read me and ask any questions you have.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RelayBot---Discord.js
 Simple Discord Relay Bot
+
+Configure the config.json
+
+<prefix> Needs to be the prefix you wish to use.  I normally use %%
+<token> This needs to be replaced by your discord token
+<RequiredRole> This is a role you give people on the server who are AUTHORIZED to use the bot. (Security Feature?)
+
+
+The purpose of this bot is to take anything typed in 1 channel and repost it (From the Bot) to another channel
+of your choice.
+
+Example
+#BotChannel - <prefix><#generalchats ID> This is from the bot
+#GeneralChats - Bot "This if from the bot"
+
+NOTE: There has to be a space between the <channel ID> and the <message>  The bot will yell at you if you get it wrong :P
---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { 
+    prefix,
+    token,
+    RequiredRoleRole
+} = require('./config.json');
+
+client.once('ready', () => {
+    console.log('RelayBot');
+    console.log('Version 1.0.1');
+});
+client.on('message', message => {
+    if(!message.content.startsWith(prefix)) return;
+//Next Line Checks to see if member has the correct "Role" assigned.
+//If the author has the correct role it will attempt to execute the command
+    if(message.member.roles.cache.find(r => r.name === `${RequiredRole}`)) {
+        
+		//attempt to catch any errors
+		try{
+        var args = message.content.split(prefix)[1].split(' ');
+        const chanid = args[0];
+        const msg = args.shift(0);
+        const msgstring = args.join(' ');
+        client.channels.cache.get(`${chanid}`).send(`${msgstring}`);
+        } catch {
+                try{
+                    var args = message.content.split(' ');
+                    const chanid = args[1];
+                    const msg = args.splice(0,2);
+                    const msgstring = args.join(' ');
+                    client.channels.cache.get(`${chanid}`).send(`${msgstring}`);
+                } catch {
+            console.log("ERROR, " + message.author + " tried to crash the bot");
+            message.reply("Follow the rules and quit trying to break me. \n I just saved you the embarassment of breaking me... you're welcome.");
+            message.channel.send("Use the correct format <Prefix><ChannelID> <SPACE> Message");
+            message.channel.send("Example: EE123412341234 This is the message");
+                }
+        }
+    };
+});
+client.login(token);
+//This bot was created by YouFoundMe#0001
+//https://github.com/Youfoundme001/RelayBot---Discord.js.
